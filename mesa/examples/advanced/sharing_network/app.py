1c2f76af7bcd00be8dab4bbb1aa0c991bd97806d
Kevin Clemons <kclemons96@hotmail.com>
2025-09-20 16:12:52 -0500

    Updated models to work with integration branch of mesa.

---
+++ mesa/examples/advanced/sharing_network/app.py
@@ -0,0 +1,143 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import solara
+from matplotlib.figure import Figure
+import sys
+sys.path.insert(0, 'C:\\Users\\Jabari\\Documents\\Git\\mesaCyberAgent')
+
+from mesa.examples.advanced.sharing_network.model import DefenderNetworkModel
+from mesa.visualization import SolaraViz
+from mesa.visualization.utils import update_counter
+import random
+
+# TODO: update model params
+model_params = {
+    "num_groups": {
+        "type": "SliderInt",
+        "value": 7,
+        "label": "Number of groups",
+        "min": 1,
+        "max": 20,
+        "step": 1,
+    },
+    "num_members": {
+        "type": "SliderInt",
+        "value": 6,
+        "label": "Number of members per group",
+        "min": 1,
+        "max": 20,
+        "step": 1,
+    },
+    "prob_add_friend": {
+        "type": "SliderFloat",
+        "label": "probability to add friend",
+        "value": 0.5,
+        "min": 0.0,
+        "max": 1.0,
+        "step": 0.1,
+    },
+    "punish": {
+        "type": "SliderFloat",
+        "label": "probability to punish",
+        "value": 0.0,
+        "min": 0.0,
+        "max": 1.0,
+        "step": 0.1,
+    },
+    "alphaM": {
+        "type": "SliderInt",
+        "value": 45,
+        "label": "alphaM",
+        "min": 1,
+        "max": 75,
+        "step": 1,
+    },
+    "betaM": {
+        "type": "SliderInt",
+        "value": 45,
+        "label": "betaM",
+        "min": 1,
+        "max": 75,
+        "step": 1,
+    },
+    "punish_cost": {
+        "type": "SliderInt",
+        "value": 50,
+        "label": "Amount punished for",
+        "min": 1,
+        "max": 500,
+        "step": 10,
+    },
+    "penalty": {
+        "type": "SliderInt",
+        "value": 100,
+        "label": "penalty amount",
+        "min": 1,
+        "max": 500,
+        "step": 10,
+    },
+    "seed": {
+        "type": "InputText",
+        "value": 42,
+        "label": "Random Seed",
+    },
+}
+
+# Create visualization elements. The visualization elements are solara components
+# that receive the model instance as a "prop" and display it in a certain way.
+# Under the hood these are just classes that receive the model instance.
+# You can also author your own visualization elements, which can also be functions
+# that receive the model instance and return a valid solara component.
+
+# Create initial model instance
+model = DefenderNetworkModel(7, 6, 0.5, 0, 45, 45, 50, 100, 42)
+g = model.network
+list(g.nodes)
+@solara.component
+def plot_network(model):
+    update_counter.get()
+    g = model.network
+    pos = nx.kamada_kawai_layout(g, dist=None, pos=None, weight='weight', scale=1, center=None, dim=2)
+    fig = Figure()
+    ax = fig.subplots()
+    labels = {agent.unique_id: agent.unique_id for agent in model.agents}
+    #TODO find issue in agent model
+    
+    colors = []
+    sizes  = []
+
+    # We append a random color/size in the beginning to account for the root node
+    colors.append(random.uniform(1, 100))
+    sizes.append(random.uniform(1, 10))
+
+    colors.extend([agent.altruism for agent in model.agents])
+    sizes.extend([agent.wealth/20 for agent in model.agents])
+    
+
+    nx.draw(
+        g,
+        pos,
+        node_size=sizes,
+        node_color=colors,
+        cmap=plt.cm.coolwarm,
+        labels=labels,
+        ax=ax,
+    )
+
+    solara.FigureMatplotlib(fig)
+
+
+
+
+# Create the SolaraViz page. This will automatically create a server and display the
+# visualization elements in a web browser.
+# Display it using the following command in the example directory:
+# solara run app.py
+# It will automatically update and display any changes made to this file
+page = SolaraViz(
+    model,
+    components=[plot_network],
+    model_params=model_params,
+    name="Sharing Network Model",
+)
+page  # noqa
---
+++ mesa/examples/advanced/sharing_network/model.py
@@ -0,0 +1,142 @@
+import matplotlib.pyplot as plt
+import math
+import random
+import decimal
+import networkx as nx
+import numpy as np
+from itertools import combinations
+
+import mesa
+from mesa import Model
+from mesa.examples.advanced.sharing_network.agents import Defender
+#from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
+#from itertools import combination
+from mesa.space import NetworkGrid
+
+#probability of attack, impact, cost to attacker, and index number, respectively, for 4 attack types
+#1:malware, 2:web-based, 3:DOS, 4:malicious insider
+attacks = [[0.25, 2600, 50, 0],[0.20, 2300, 60, 1], [0.20, 1700, 70, 2], [0.15, 1600, 65, 3]] 
+att_combos = list()
+for n in range(len(attacks) + 1):
+    att_combos += list(combinations(attacks, n))
+att_combos.remove(())
+
+#prob of mitigating attack 1-4, investment, and annual cost, respectively, for 6 counter measure types
+#1:security sharing, 2:access management, 3:cyber behavior analytics, 4:cryptography, 5:policy management, 6:enterprise governance
+countermeasure = [[0.6, 0.5, 0.4, 0.5,   100, 25],[0.4, 0.6, 0.4, 0.6, 80, 30],[0.5, 0.5, 0.4, 0.6, 110, 30],[0.4, 0.5, 0.3, 0.4, 100, 5],[0.5, 0.4, 0.3, 0.5, 80, 45],[0.5, 0.5, 0.4, 0.5, 300, 50]]
+cm_combos = list()
+for n in range(len(countermeasure) + 1):
+    cm_combos += list(combinations(countermeasure, n))
+cm_combos.remove(())
+#cm_combos[combo set][countermeasure][value]
+
+class DefenderNetworkModel(mesa.Model):
+    """A model with some number of agents."""
+    """add description of network, change class names"""
+
+    def __init__(self, num_groups=7, num_members=6, prob_add_friend=0.5, punish = 0, alphaM=45, betaM=45, punish_cost=50, penalty=100, seed=42):
+
+        super().__init__(seed=seed)
+        self.num_agents = num_groups*num_members
+        self.num_nodes = self.num_agents
+        self.network = nx.planted_partition_graph(num_groups, num_members, 0.5, 0.1, seed=seed)
+        #self.network = nx.Graph()
+        self.G = nx.planted_partition_graph(num_groups, num_members, 0.5, 0.1, seed=seed)
+        self.grid = NetworkGrid(self.network)
+        # TODO schedule may no longer be necessary
+        #self.schedule = RandomActivation(self)
+        self.rosi = 0
+        self.datacollector = DataCollector(
+            model_reporters={"prob_of_contr": self.prob_of_contr, "gain": self.gain, "wealth": self.wealth, "freeriders":self.freeriders, "friends":self.friends, "altruism":self.altruism, "contribution_value":self.contribution_value, "cmlist":self.cmlist, "attlist":self.attlist,  "graph": self.graph, "alpha": self.alpha, "beta": self.beta},
+            agent_reporters={"Contribution %": lambda _: _.prob_of_contr, "Altruism": lambda _: _.gain}
+        )
+
+        argies = [prob_add_friend, punish, alphaM, betaM, punish_cost, penalty]
+        Defender.create_agents(self, self.num_agents, prob_add_friend, punish, alphaM, betaM, punish_cost, penalty)
+
+        ##list_of_random_nodes = self.random.sample(self.G.nodes(), self.num_agents)
+        agent_ids = [
+            (agent.unique_id, {"size": 300, "level": 0}) for agent in self.agents
+        ]
+        self.network.add_nodes_from(agent_ids)
+
+        # Create agents
+        #TODO redo agent creation
+        """
+        for i in range(self.num_agents):
+            a = Defender(i, self, prob_add_friend, punish, alphaM, betaM, punish_cost, penalty)
+            self.network.add_nodes_from(agent_ids)
+            #self.schedule.add(a)
+            # Add the agent to a random node
+            self.grid.place_agent(a, list_of_random_nodes[i])
+        """
+
+        self.running = True
+        self.datacollector.collect(self)
+
+    def step(self):
+        for agent in self.agents:
+            agent.step()
+        # collect data
+        self.datacollector.collect(self)
+        
+    def datacollect(self):
+        return self.datacollector.get_model_vars_dataframe()
+        
+    def prob_of_contr(self):
+        y = [agent.prob_of_contr for agent in self.agents]
+        #x = [agent.prob_of_contr <= 0 for agent in self.agents]
+        return y
+    
+    def wealth(self): 
+        y = [agent.wealth for agent in self.agents]
+        #x = [agent.prob_of_contr <= 0 for agent in self.agents]
+        return y
+    
+    def gain(self):
+        y = [agent.gain for agent in self.agents]
+        #x = [agent.prob_of_contr <= 0 for agent in self.agents]
+        return y
+    
+    def freeriders(self):
+        y = [agent.k for agent in self.agents]
+        #x = [agent.prob_of_contr <= 0 for agent in self.agents]
+        return y
+    
+    def friends(self):
+        y = [len(agent.friends) for agent in self.agents]
+        #x = [agent.prob_of_contr <= 0 for agent in self.agents]
+        return y
+    
+    def altruism(self):
+        y = [agent.altruism for agent in self.agents]
+        #x = [agent.prob_of_contr <= 0 for agent in self.agents]
+        return y
+    
+    def contribution_value(self):
+        y = [agent.cmValue  + agent.attValue for agent in self.agents]
+        return y
+    
+    def cmlist(self):
+        y = [agent.cmlist for agent in self.agents]
+        return y
+    
+    def attlist(self):
+        y = [agent.attlist for agent in self.agents]
+        return y
+    
+    def alpha(self):
+        y = [agent.alpha for agent in self.agents]
+        return y
+    
+    def beta(self):
+        y = [agent.beta for agent in self.agents]
+        return y
+    
+    def graph(self):
+        return self.G
+
+    def run_model(self, n):
+        for i in range(n):
+            self.step()
